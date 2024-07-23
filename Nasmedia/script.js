c2130c9c42dd325a7b0a37fa51c7048023b83a41
FeisyahAzzahra <127309541+FeisyahAz-zahra@users.noreply.github.com>
2024-07-23 19:12:17 +0700

    update navbar

--- Nasmedia/script.js
+++ Nasmedia/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toggle = document.getElementById('nav-toggle');
+    const toggle = document.getElementById('hamburger');
     const menu = document.getElementById('nav-menu');
   
     toggle.addEventListener('click', function() {
