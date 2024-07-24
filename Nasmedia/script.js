14e9d80a7b4fdeacbf7bd518fbd22d1f32f7a54d
LiskaAyuningsih <liskaayuningsih09@gmail.com>
2024-07-24 17:32:04 +0700

    Update script.js

--- Nasmedia/script.js
+++ Nasmedia/script.js
@@ -1,9 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toggle = document.getElementById('hamburger');
-    const menu = document.getElementById('nav-menu');
-  
-    toggle.addEventListener('click', function() {
-      menu.classList.toggle('active');
-    });
+  const toggle = document.getElementById('hamburger');
+  const menu = document.getElementById('nav-menu');
+
+  toggle.addEventListener('click', function() {
+    menu.classList.toggle('active');
   });
-  
+});
