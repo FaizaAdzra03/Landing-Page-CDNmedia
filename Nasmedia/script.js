db94cf0538f0f57c3532c1d6c8a96611312fc082
FaizaAdzra03 <90081266+FaizaAdzra03@users.noreply.github.com>
2024-07-31 21:32:36 +0700

    Delete Nasmedia directory

--- Nasmedia/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const toggle = document.getElementById('hamburger');
-  const menu = document.getElementById('nav-menu');
-
-  toggle.addEventListener('click', function() {
-    menu.classList.toggle('active');
-  });
-});
